d8d0c48b1574b9aaab36e72c5f6ef1949053d3ad
cssxsh <cssxsh@gmail.com>
2020-09-09 23:30:16 +0800

    style: build.gradle.kts

--- code02/build.gradle.kts
+++ code02/build.gradle.kts
@@ -22,13 +22,18 @@ android {
     buildTypes {
         getByName("release") {
             minifyEnabled(false)
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
-        sourceCompatibility =  JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
     kotlinOptions {
         jvmTarget = "1.8"
     }
