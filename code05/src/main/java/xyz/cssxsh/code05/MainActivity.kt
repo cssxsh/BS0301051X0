9f7dc54a80b171b93df4bd49f3cfccba034bb7c3
cssxsh <cssxsh@gmail.com>
2020-09-09 10:38:41 +0800

    version: code05

--- code05/src/main/java/xyz/cssxsh/code05/MainActivity.kt
+++ code05/src/main/java/xyz/cssxsh/code05/MainActivity.kt
@@ -2,43 +2,28 @@ package xyz.cssxsh.code05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import xyz.cssxsh.code02.R
+import android.text.InputType
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
 
-    companion object {
-        @JvmStatic
-        private val COUNT_VALUE = "${this::class.qualifiedName}.COUNT_VALUE"
-    }
+    private var seePassword = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnShowToast)?.setOnClickListener {
-            Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+        see_switch.setOnClickListener {
+            seePassword = !seePassword
+            if (seePassword) {
+                password.inputType =
+                    InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                see_switch.setImageResource(R.drawable.ic_baseline_visibility_24)
+            } else {
+                password.inputType =
+                    InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+                see_switch.setImageResource(R.drawable.ic_baseline_visibility_off_24)
+            }
         }
-
-        findViewById<Button>(R.id.btnCount)?.setOnClickListener {
-            findViewById<TextView>(R.id.tvCount)?.text = (++count).toString()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        findViewById<TextView>(R.id.tvCount)?.text = count.toString()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState.apply { putInt(COUNT_VALUE, count) })
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState.also { count = it.getInt(COUNT_VALUE) })
-        findViewById<TextView>(R.id.tvCount)?.text = count.toString()
     }
 }
